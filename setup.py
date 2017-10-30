bba1e56c718a7facd38bb7e4c20937001f1aa65a
Tom <tmbdev@gmail.com>
2017-10-30 10:43:09 -0700

    added missing files

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ from __future__ import print_function
 
 import sys,time,urllib,traceback,glob,os,os.path
 
+os.system("wget -nc -nd https://storage.googleapis.com/tmbmodels/ocr-default.pt")
+
 assert sys.version_info[0]==2 and sys.version_info[1]>=7,\
     "you must install and use OCRopus with Python version 2.7 or later, but not Python 3.x"
 
 from distutils.core import setup #, Extension, Command
 #from distutils.command.install_data import install_data
 
-models = [c for c in glob.glob("models/*pyrnn.gz")]
+models = ["ocr-default.pt"]
 scripts = [c for c in glob.glob("ocropus-*") if "." not in c and "~" not in c]
 scripts += [c for c in glob.glob("ocroline-*") if "." not in c and "~" not in c]
 scripts += [c for c in glob.glob("ocroseg-*") if "." not in c and "~" not in c]
-models, scripts = [], []
 
 setup(
     name = 'ocropy2',
@@ -22,6 +23,6 @@ setup(
     author = "Thomas Breuel",
     description = "The OCRopus2 Python Library and Tools.",
     packages = ["ocropy2"],
-    data_files= [('share/ocropus', models)],
+    data_files= [('share/ocropy2', models)],
     scripts = scripts,
     )

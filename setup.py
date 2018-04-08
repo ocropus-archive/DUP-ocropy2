092751408a5ded315554c3d6392af369ffd53953
Thomas Breuel <tbreuel@nvidia.com>
2018-04-08 16:56:11 -0700

    commented out model installation

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ from __future__ import print_function
 
 import sys,time,urllib,traceback,glob,os,os.path
 
-os.system("wget -nc -nd https://storage.googleapis.com/tmbmodels/ocr-default.pt")
+if 0:
+    os.system("wget -nc -nd https://storage.googleapis.com/tmbmodels/ocr-default.pt")
+    models = ["ocr-default.pt"]
+else:
+    models = []
 
 assert sys.version_info[0]==2 and sys.version_info[1]>=7,\
     "you must install and use OCRopus with Python version 2.7 or later, but not Python 3.x"
@@ -12,7 +16,7 @@ assert sys.version_info[0]==2 and sys.version_info[1]>=7,\
 from distutils.core import setup #, Extension, Command
 #from distutils.command.install_data import install_data
 
-models = ["ocr-default.pt"]
+
 scripts = [c for c in glob.glob("ocropus-*") if "." not in c and "~" not in c]
 scripts += [c for c in glob.glob("ocroline-*") if "." not in c and "~" not in c]
 scripts += [c for c in glob.glob("ocroseg-*") if "." not in c and "~" not in c]
